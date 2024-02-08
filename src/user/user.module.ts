e7a2835176963db3d2033885e8cd12f9d4451801
Abdelmounaim-Abounore <abdelmounaim.abounore@gmail.com>
2024-02-08 13:53:18 +0100

    add validation for register function

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,14 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './entities/user.entity/user.entity';
+import { Role, RoleSchema } from './entities/role.entity/role.entity';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema}])],
+  imports: [MongooseModule.forFeature([
+    { name: User.name, schema: UserSchema},
+    { name: Role.name, schema: RoleSchema },
+
+  ])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService]
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,40 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './entities/user.entity/user.entity'
 import * as bcryptjs from 'bcryptjs';
+import { CreateUserDto } from './dto/create.user.dto';
+import { validate } from 'class-validator';
+import { Role } from './entities/role.entity/role.entity';
+
 // import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class UserService {
-    constructor(@InjectModel(User.name) private readonly userModel: Model<User>) {}
+    constructor(
+      @InjectModel(User.name) private readonly userModel: Model<User>,
+      @InjectModel(Role.name) private readonly roleModel: Model<Role>,
+      ) {}
 
   async registerUser(userData: any): Promise<any> {
+
+    // Validate the user data using the CreateUserDto
+    const createUserDto = new CreateUserDto();
+    createUserDto.name = userData.name;
+    createUserDto.phoneNumber = userData.phoneNumber;
+    createUserDto.email = userData.email;
+    createUserDto.password = userData.password;
+    createUserDto.image = userData.image;
+    createUserDto.roleId = userData.roleId;
+
+    // Validate the DTO using class-validator
+    const validationErrors = await validate(createUserDto);
+    console.log(validationErrors)
+
+    if (validationErrors.length > 0) {
+      // Validation errors occurred, throw an exception with the details
+      throw new ConflictException(validationErrors.map(error => Object.values(error.constraints).join(', ')).join(', '));
+    }
 
     // Check if email already exists
     const emailExists = await this.userModel.findOne({ email: userData.email });
@@ -17,14 +42,24 @@ export class UserService {
       throw new NotFoundException('This Email is already exists. Try to sign in.');
     }
 
+    if (userData.password !== userData.confirmPassword) {
+      throw new ConflictException('Password and Confirm Password do not match.');
+    }
+
     // Hash the password
     const genSalt = await bcryptjs.genSalt(10);
     const hashedPassword = await bcryptjs.hash(userData.password, genSalt);
 
+    // Check if roleId exists in the roles collection
+    const roleExists = await this.roleModel.findById(userData.roleId);
+    if (!roleExists) {
+      throw new NotFoundException('Invalid roleId. Role not found.');
+    }
+
     // Create a new user instance
     const newUser = new this.userModel({
       name: userData.name,
-      phoneNumber: hashedPassword,
+      phoneNumber: userData.phoneNumber,
       email: userData.email,
       password: hashedPassword,
       image: userData.image,
@@ -32,11 +67,12 @@ export class UserService {
     });
 
     // Save the new user
-    try {
-      const savedUser = await newUser.save();
-      let userData = savedUser.toObject();
+    const savedUser = await newUser.save();
+    let userObject = savedUser.toObject();
 
-      delete userData.password;
+    // Remove the password from the response
+    delete userObject.password;
+
 
       // Generate and send activation email
     //   const token = jwt.sign(userData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10m' });
@@ -79,4 +115,4 @@ export class UserService {
       throw new NotFoundException(err);
     }
   }
-}
+
